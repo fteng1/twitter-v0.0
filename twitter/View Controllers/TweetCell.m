742234f3c647a2188a9941257f8cf0e6e914712f
Felianne Teng <felianneteng@fb.com>
2021-06-29 14:32:36 -0700

    Favorite and retweet

--- twitter/View Controllers/TweetCell.m
+++ twitter/View Controllers/TweetCell.m
@@ -7,13 +7,124 @@
 //
 
 #import "TweetCell.h"
+#import "APIManager.h"
 
 @implementation TweetCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    [self.retweetButton setImage:[UIImage imageNamed:@"retweet-icon-green"] forState:UIControlStateSelected];
+    [self.retweetButton setImage:[UIImage imageNamed:@"retweet-icon"] forState:UIControlStateNormal];
+    [self.favButton setImage:[UIImage imageNamed:@"favor-icon-red"] forState:UIControlStateSelected];
+    [self.favButton setImage:[UIImage imageNamed:@"favor-icon"] forState:UIControlStateNormal];
+    
+    [self refreshData];
 }
+
+- (void)refreshData {
+    User *user = self.tweet.user;
+    self.authorLabel.text = user.name;
+    self.usernameLabel.text = user.screenName;
+    self.dateLabel.text = self.tweet.createdAtString;
+    self.favLabel.text = [NSString stringWithFormat:@"%d", self.tweet.favoriteCount];
+    self.retweetLabel.text = [NSString stringWithFormat:@"%d", self.tweet.retweetCount];
+    self.tweetLabel.text = self.tweet.text;
+    self.favButton.selected = self.tweet.favorited;
+    self.retweetButton.selected = self.tweet.retweeted;
+    
+    // get profile image
+    NSString *URLString = self.tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    UIImage *image = [UIImage imageWithData:urlData];
+    [self.profileImageView setImage:image];
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    if (!self.tweet.favorited) {
+        // TODO: Update the local tweet model
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        self.favButton.selected = YES;
+        
+        // TODO: Update cell UI
+        [self refreshData];
+        
+        // TODO: Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        // Update the local tweet model
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        self.favButton.selected = NO;
+        
+        // Update cell UI
+        [self refreshData];
+        
+        // Send a POST request
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+
+    }
+}
+
+- (IBAction)didTapRetweet:(id)sender {
+    if (!self.tweet.retweeted) {
+        // TODO: Update the local tweet model
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        self.retweetButton.selected = YES;
+        
+        // TODO: Update cell UI
+        [self refreshData];
+        
+        // TODO: Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        // Update the local tweet model
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        self.retweetButton.selected = NO;
+        
+        // Update cell UI
+        [self refreshData];
+        
+        // Send a POST request
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+
+    }
+}
+
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
